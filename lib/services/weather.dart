d91e2b2600b29b8f0546fee495f6c77c09fdf2f8
matheusbruns <matheus.rbruns@gmail.com>
2022-10-28 00:46:03 -0300

    initial commit

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,24 @@
+import 'package:tempo_template/services/location.dart';
+import 'package:tempo_template/services/networking.dart';
+
+const apiKey = 'd7636044ca5e91285f92163aea7dc351';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+
+  Future<dynamic> getLocationWeather() async {
+
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}'
+            '&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
